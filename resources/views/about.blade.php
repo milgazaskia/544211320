699f4a40dcc03cb41e5d67b6a5128e6eae40ecf9
milgazaskia <wdmilgazaskia018@gmail.com>
2024-01-28 21:34:00 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+{{-- ngambil dari halaman utama --}}
+@extends('layouts.main') 
+
+{{-- tulisan dibawah ini untuk akan mengambil isi halaman dari section(container) --}}
+@section('container') 
+<h1>halaman about</h1>
+@endsection
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,7 @@
+{{-- ngambil dari halaman utama --}}
+@extends('layouts.main') 
+
+{{-- tulisan dibawah ini untuk akan mengambil isi halaman dari section(container) --}}
+@section('container') 
+<h1>halaman blog</h1>
+@endsection
